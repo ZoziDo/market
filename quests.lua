dbeeb79fc3b9ab87d374c37adfa25578b298739f
ZoziDo <163629061+ZoziDo@users.noreply.github.com>
2026-05-27 11:29:19 +0700

    Create quests.lua

---
+++ quests.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    name = "Амортенция",
+    priceEma = 95,
+    requiredItems = {
+      { displayName = "Зелье вреда II",        internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье вреда I",         internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье замедления",      internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье замедления",      internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье невидимости",     internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье невидимости",     internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье ночного зрения",  internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье ночного зрения",  internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье огнестойкости",   internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье огнестойкости",   internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье отравления",      internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье отравления",      internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье подводного дыхания", internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье подводного дыхания", internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье регенерации",     internalName = "minecraft:potion", damage = 0, requiredCount = 26 },
+      { displayName = "Зелье регенерации",     internalName = "minecraft:potion", damage = 0, requiredCount = 26 },
+      { displayName = "Зелье регенерации",     internalName = "minecraft:potion", damage = 0, requiredCount = 26 },
+      { displayName = "Зелье силы",            internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье силы",            internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье силы",            internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье слабости",        internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье слабости",        internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье стремительности", internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье стремительности", internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Зелье стремительности", internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Лечебное зелье",        internalName = "minecraft:potion", damage = 0, requiredCount = 25 },
+      { displayName = "Лечебное зелье",        internalName = "minecraft:potion", damage = 0, requiredCount = 25 }
+    }
+  }
+  -- сюда можно добавить другие квесты
+}
