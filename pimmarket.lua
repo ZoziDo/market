74cd74ead3a5a64676f3e15b5bc3dd4dedae1b54
ZoziDo <163629061+ZoziDo@users.noreply.github.com>
2026-07-01 19:25:36 +0700

    Update pimmarket.lua

--- pimmarket.lua
+++ pimmarket.lua
@@ -33,7 +33,7 @@ local function getRealTimeHM()
     return os.date("%H:%M:%S", getRealTimestamp())
 end
 
-local serverAddress = "1ffb2253-b1b6-4df5-b33a-b627ec9bb4fe"
+local serverAddress = "2a904dc5-cbac-47de-be90-6f407fa91ecc"
 local ACCESS_PASSWORD = "secret"
 
 local colors = {
